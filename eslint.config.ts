56908f33899a0096548f3d40af50884579cc736f
quantium-rock <alexcollsoutumuro@gmail.com>
2026-01-21 11:21:02 +0100

    🔧 Update ESLint and package configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
-export default tseslint.config(
+export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
@@ -27,64 +27,11 @@ export default tseslint.config(
       },
     },
     rules: {
-      // Semicolons required
-      'semi': ['error', 'always'],
-      '@typescript-eslint/semi': ['error', 'always'],
-
-      // 2 spaces indentation
-      'indent': 'off',
-      '@typescript-eslint/indent': ['error', 2, {
-        SwitchCase: 1,
-        VariableDeclarator: 1,
-        outerIIFEBody: 1,
-        MemberExpression: 1,
-        FunctionDeclaration: { parameters: 1, body: 1 },
-        FunctionExpression: { parameters: 1, body: 1 },
-        CallExpression: { arguments: 1 },
-        ArrayExpression: 1,
-        ObjectExpression: 1,
-        ImportDeclaration: 1,
-        flatTernaryExpressions: false,
-        ignoreComments: false,
-      }],
-
-      // Max 100 characters per line
-      'max-len': ['error', {
-        code: 100,
-        tabWidth: 2,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-        ignoreComments: false,
-      }],
-
-      // Quotes
-      'quotes': 'off',
-      '@typescript-eslint/quotes': ['error', 'single', { avoidEscape: true }],
-
-      // Trailing commas
-      'comma-dangle': 'off',
-      '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
-
       // No unused vars (allow underscore prefix)
       '@typescript-eslint/no-unused-vars': ['error', {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
       }],
-
-      // Spacing
-      'object-curly-spacing': ['error', 'always'],
-      'array-bracket-spacing': ['error', 'never'],
-      'space-before-function-paren': ['error', {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always',
-      }],
-      'keyword-spacing': ['error', { before: true, after: true }],
-      'space-infix-ops': 'error',
-      'comma-spacing': ['error', { before: false, after: true }],
-      'no-multi-spaces': 'error',
 
       // Best practices
       'eqeqeq': ['error', 'always'],
@@ -103,4 +50,4 @@ export default tseslint.config(
       }],
     },
   },
-);
+];
